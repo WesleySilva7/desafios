c261c9074835ce17ec8cad2c6d37b5469d1fab6e
WesleySilva7 <70494266+WesleySilva7@users.noreply.github.com>
2023-04-07 23:07:29 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,14 @@
+# AQUI É DECLARADO AS VÁRIAVEIS
+INDICE = 13
+SOMA = 0
+K = 0 
+
+# While loop para somar os números de 1 a 13.
+
+while K < INDICE:
+    K += 1
+    SOMA += K
+
+# AQUI IRÁ EXIBIR O RESULTADO
+print('O resultado da soma é',SOMA)
+
